7b9d347022623edc3ffceedd9989b8896fc42878
yeyeyec <flyinglotus01@163.com>
2022-06-02 13:53:11 +0800

    completed first part of lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,14 @@
+public class HelloNumbers{
+    public static void main(String[] args){
+        int x;
+        int sum;
+        x = 0;
+        sum = 0;
+        while (x<10){
+            System.out.println(sum);
+            x = x +1;
+            sum = sum + x;
+
+        }
+    }
+}
